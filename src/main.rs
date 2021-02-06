be7cb00dd4751bfacb9cca985acdb56da4628d10
chansuke <chansuke@georepublic.de>
2021-02-06 11:55:15 +0900

    Start creating repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use rustyline::Editor;
+use anyhow::Result;
+
+fn main() -> Result<()>{
+    let mut rl = Editor::<()>::new();
+
+    loop {
+        let readline = rl.readline("rsqlite> ");
+        match readline {
+            Ok(line) => {
+                if line == ".exit" {
+                    println!("Bye");
+                    break
+                } else {
+                    println!("Unrecognized command {:?}", line);
+                }
+            },
+            Err(err) => {
+                println!("Error {:?}", err);
+                break
+            }
+        }
+    }
+
+    Ok(())
 }
