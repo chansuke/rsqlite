c3e28beda1ec6f79fe2374c27352cb7e64f3ba1b
chansuke <chansuke@georepublic.de>
2021-05-12 20:04:55 +0900

    Scrap and build

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rl = Editor::<()>::new();
+
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+
+    loop {
+        let readline = rl.readline("db >> ");
+        match readline {
+            Ok(line) => {
+                if line == ".exit" {
+                    break;
+                } else {
+                    println!("Unrecognized command {:?}", line);
+                }
+            }
+            Err(err) => {
+                println!("Error reading input");
+                break;
+            }
+        }
+    }
 }
